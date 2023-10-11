a5f8b720e4d9c5eaff65ada25fdc7390a3944773
Alex Standiford <alex@standiford.us>
2023-10-11 10:48:50 -0400

    Updates WordPressInitializer to utilize new initializer pattern

--- lib/Strategies/WordPressInitializer.php
+++ lib/Strategies/WordPressInitializer.php
@@ -5,15 +5,16 @@ namespace Phoenix\Integrations\WordPress\Strategies;
 use Phoenix\Cache\Interfaces\CacheStrategy;
 use Phoenix\Cache\Interfaces\InMemoryCacheStrategy;
 use Phoenix\Cache\Interfaces\PersistentCacheStrategy;
-use Phoenix\Core\Bootstrap\Interfaces\Initializer;
+use Phoenix\Core\Bootstrap\Interfaces\HasClassDefinitions;
 use Phoenix\Events\Interfaces\EventStrategy as CoreEventStrategy;
 use Phoenix\Database\Interfaces\QueryBuilder as CoreQueryBuilder;
 use Phoenix\Integrations\WordPress\Cache\ObjectCacheStrategy;
 use Phoenix\Integrations\WordPress\Cache\TransientCacheStrategy;
 use Phoenix\Integrations\WordPress\Database\QueryBuilder;
+use Phoenix\Loader\Interfaces\HasLoadCondition;
 use Phoenix\Rest\Interfaces\RestStrategy as CoreRestStrategy;
 
-class WordPressInitializer implements Initializer
+class WordPressInitializer implements HasLoadCondition, HasClassDefinitions
 {
     public const REQUIRED_WORDPRESS_VERSION = '6.3.1';
 
@@ -36,20 +37,10 @@ class WordPressInitializer implements Initializer
      *
      * @return bool
      */
-    public function requirementsMet(): bool
+    public function shouldLoad(): bool
     {
         global $wp_version;
 
         return isset($wp_version) && version_compare($wp_version, static::REQUIRED_WORDPRESS_VERSION, '>=');
-    }
-
-    public function init(): void
-    {
-        // No-op
-    }
-
-    public function getConfigDirectories(): array
-    {
-        return [];
     }
 }
