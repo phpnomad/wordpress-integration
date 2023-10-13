35a1e9a583e2d518bb3e8eb5ea0e8ac7137a561f
Alex Standiford <alex@standiford.us>
2023-10-13 08:55:33 -0400

    Updates dependencies based on core changes

--- lib/Strategies/WordPressInitializer.php
+++ lib/Strategies/WordPressInitializer.php
@@ -5,9 +5,8 @@ namespace Phoenix\Integrations\WordPress\Strategies;
 use Phoenix\Cache\Interfaces\CacheStrategy;
 use Phoenix\Cache\Interfaces\InMemoryCacheStrategy;
 use Phoenix\Cache\Interfaces\PersistentCacheStrategy;
-use Phoenix\Core\Bootstrap\Interfaces\HasClassDefinitions;
+use Phoenix\Database\Interfaces\DatabaseStrategy as CoreDatabaseStrategy;
 use Phoenix\Database\Interfaces\HasDatabaseDefaultCacheTtl;
-use Phoenix\Database\Interfaces\HasDatabasePrefix;
 use Phoenix\Database\Interfaces\HasGlobalDatabasePrefix;
 use Phoenix\Events\Interfaces\EventStrategy as CoreEventStrategy;
 use Phoenix\Database\Interfaces\QueryBuilder as CoreQueryBuilder;
@@ -15,6 +14,7 @@ use Phoenix\Integrations\WordPress\Cache\ObjectCacheStrategy;
 use Phoenix\Integrations\WordPress\Cache\TransientCacheStrategy;
 use Phoenix\Integrations\WordPress\Database\QueryBuilder;
 use Phoenix\Integrations\WordPress\Providers\DatabaseProvider;
+use Phoenix\Loader\Interfaces\HasClassDefinitions;
 use Phoenix\Loader\Interfaces\HasLoadCondition;
 use Phoenix\Rest\Interfaces\RestStrategy as CoreRestStrategy;
 
@@ -31,6 +31,7 @@ class WordPressInitializer implements HasLoadCondition, HasClassDefinitions
             EventStrategy::class => CoreEventStrategy::class,
             TransientCacheStrategy::class => PersistentCacheStrategy::class,
             ObjectCacheStrategy::class => [CacheStrategy::class, InMemoryCacheStrategy::class],
+            DatabaseStrategy::class => CoreDatabaseStrategy::class,
             QueryBuilder::class => CoreQueryBuilder::class,
             RestStrategy::class => CoreRestStrategy::class,
             DatabaseProvider::class => [HasDatabaseDefaultCacheTtl::class, HasGlobalDatabasePrefix::class]
