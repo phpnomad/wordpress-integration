bba7e243d509fb2cfe113ece83dd48c90011c1e4
Alex Standiford <alex@standiford.us>
2023-11-08 06:39:03 -0600

    corrected strategy return type

--- lib/Strategies/MutationStrategy.php
+++ lib/Strategies/MutationStrategy.php
@@ -6,7 +6,7 @@ use PHPNomad\Mutator\Interfaces\MutationStrategy as MutationStrategyInterface;
 
 class MutationStrategy implements MutationStrategyInterface
 {
-    public function attach(callable $mutatorGetter, callable $action): void
+    public function attach(callable $mutatorGetter, string $action): void
     {
         add_filter($action, fn(...$args) => $mutatorGetter()->mutate(...$args));
     }
