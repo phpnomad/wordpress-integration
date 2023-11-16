2fbda28de8c8dbb05f9b78e4d17eeb29b481f20c
Alex Standiford <alex@standiford.us>
2023-11-16 09:39:40 -0600

    progress

---
+++ lib/Strategies/TranslationStrategy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PHPNomad\Integrations\WordPress\Strategies;
+
+use PHPNomad\Integrations\WordPress\Interfaces\HasTextDomain;
+use PHPNomad\Translations\Interfaces\TranslationStrategy as TranslationStrategyInterface;
+
+class TranslationStrategy implements TranslationStrategyInterface
+{
+    protected HasTextDomain $textDomainProvider;
+
+    public function __construct(HasTextDomain $textDomainProvider)
+    {
+        $this->textDomainProvider = $textDomainProvider;
+    }
+
+    /** @inheritDoc */
+    public function translate(string $translate, ?string $language = null, $context = null): string
+    {
+        if ($language !== null) {
+            switch_to_locale($language);
+        }
+
+        $domain = $this->textDomainProvider->getTextDomain();
+        $translated = $context === null ? __($translate, $domain) : _x($translate, $domain);
+
+        if ($language !== null) {
+            restore_previous_locale();
+        }
+
+        return $translated;
+    }
+}
