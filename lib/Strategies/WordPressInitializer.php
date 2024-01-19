62ba7a1e4d7789ec9b2983ea06d261e7d8e5cb64
Alex Standiford <alex@standiford.us>
2024-01-19 10:55:49 -0500

    Progress

--- lib/Strategies/WordPressInitializer.php
+++ lib/Strategies/WordPressInitializer.php
@@ -15,15 +15,17 @@ use PHPNomad\Database\Interfaces\QueryStrategy as CoreQueryStrategy;
 use PHPNomad\Database\Interfaces\TableCreateStrategy as CoreTableCreateStrategyAlias;
 use PHPNomad\Database\Interfaces\TableDeleteStrategy as CoreTableDeleteStrategyAlias;
 use PHPNomad\Database\Interfaces\TableExistsStrategy as CoreTableExistsStrategyAlias;
+use PHPNomad\Di\Exceptions\DiException;
+use PHPNomad\Di\Interfaces\CanSetContainer;
+use PHPNomad\Di\Traits\HasSettableContainer;
 use PHPNomad\Events\Interfaces\EventStrategy as CoreEventStrategy;
 use PHPNomad\Events\Interfaces\HasEventBindings;
-use PHPNomad\Events\Interfaces\HasListeners;
+use PHPNomad\Framework\Events\SiteVisited;
 use PHPNomad\Integrations\WordPress\Adapters\DatabaseDateAdapter;
 use PHPNomad\Integrations\WordPress\Cache\CachePolicy;
 use PHPNomad\Cache\Interfaces\CachePolicy as CoreCachePolicy;
 use PHPNomad\Integrations\WordPress\Cache\ObjectCacheStrategy;
 use PHPNomad\Integrations\WordPress\Database\QueryBuilder;
-use PHPNomad\Integrations\WordPress\Listeners\FrontEndRequestListener;
 use PHPNomad\Integrations\WordPress\Providers\DatabaseProvider;
 use PHPNomad\Integrations\WordPress\Providers\DefaultCacheTtlProvider;
 use PHPNomad\Integrations\WordPress\Rest\Response;
@@ -35,8 +37,10 @@ use PHPNomad\Rest\Interfaces\RestStrategy as CoreRestStrategy;
 use \PHPNomad\Events\Interfaces\ActionBindingStrategy as CoreActionBindingStrategy;
 use PHPNomad\Utils\Helpers\Arr;
 
-class WordPressInitializer implements HasLoadCondition, HasClassDefinitions, HasListeners, HasEventBindings
+class WordPressInitializer implements CanSetContainer, HasLoadCondition, HasClassDefinitions, HasEventBindings
 {
+    use HasSettableContainer;
+
     public const REQUIRED_WORDPRESS_VERSION = '6.3.0';
 
     /**
@@ -75,19 +79,39 @@ class WordPressInitializer implements HasLoadCondition, HasClassDefinitions, Has
         return isset($wp_version) && version_compare($wp_version, static::REQUIRED_WORDPRESS_VERSION, '>=');
     }
 
-    public function getListeners(): array
-    {
-        return [
-            FrontEndRequestListener::class
-        ];
-    }
-
     public function getEventBindings(): array
     {
         return [
             RecordDeleted::class => [
                 ['action' => 'deleted_user', 'transformer' => fn(int $id) => new RecordDeleted('user', Arr::wrap($id))]
+            ],
+            SiteVisited::class => [
+                ['action' => 'init', 'transformer' => fn() => $this->handleSiteVisitedBinding()]
             ]
         ];
+    }
+
+    /**
+     * @return void
+     * @throws DiException
+     */
+    private function handleSiteVisitedBinding(): void
+    {
+        $isNotFrontendRequest =
+            is_admin()
+            || wp_doing_ajax()
+            || defined('REST_REQUEST') && REST_REQUEST
+            || (defined('WP_CLI') && WP_CLI)
+            || defined('XMLRPC_REQUEST') && XMLRPC_REQUEST
+            || defined('DOING_CRON') && DOING_CRON;
+
+        if($isNotFrontendRequest){
+            return;
+        }
+
+        $userId = get_current_user_id();
+        $userId = $userId <= 0 ? null : $userId;
+
+        $this->container->get(CoreEventStrategy::class)->broadcast(new SiteVisited($userId));
     }
 }
